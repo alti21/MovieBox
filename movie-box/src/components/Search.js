3ae6be5bbc540c1f05fb0b8ba22031f40ca26bed
Altemush Bhatti <altemush@gmail.com>
2021-04-29 19:50:54 -0700

    feat: create search bar component

---
+++ movie-box/src/components/Search.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Search = () => {
+  return (
+    <header>
+      <input
+        type="text"
+        placeholder="Search for a movie"
+        className="search-bar"
+      />
+    </header>
+  );
+};
+
+export default Search;
